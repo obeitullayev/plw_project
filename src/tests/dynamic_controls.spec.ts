6e88110652bf15c6acd643530a03458d282018f1
obeitullaev <beyocman@gmail.com>
2025-10-12 22:23:48 +0300

    added task-1 to HW-20

---
+++ src/tests/dynamic_controls.spec.ts
@@ -0,0 +1,50 @@
+import test, {expect} from "@playwright/test";
+
+/*Разработать тест со следующими шагами:
+  - открыть https://the-internet.herokuapp.com/
+  - перейти на страницу Dynamic Controls
+  - Дождаться появления кнопки Remove
+  - Завалидировать текста в заголовке страницы
+  - Чекнуть чекбокс
+  - Кликнуть по кнопке Remove
+  - Дождаться исчезновения чекбокса
+  - Проверить наличие кнопки Add
+  - Завалидировать текст It's gone!
+  - Кликнуть на кнопку Add
+  - Дождаться появления чекбокса
+  - Завалидировать текст It's back!
+*/
+
+test.describe("[Dynamic Controls]", ()=> {
+    test("Should open page Dynamic Controls and remove and back checkbox", async ({page}) => {
+        const url = "https://the-internet.herokuapp.com/"
+        const link = page.locator("a[href='/dynamic_controls']")
+        const header =  page.getByRole('link', { name: 'Dynamic Controls' })
+        const button = page.locator("button[onclick='swapCheckbox()']")
+        const checkbox = page.locator("//input[@type='checkbox']")
+        const message= page.locator("#message");
+
+        await page.goto(url);
+        await expect(header).toHaveText('Dynamic Controls')
+        await expect(link).toBeVisible();
+
+        await link.click();
+        await expect(checkbox).toBeVisible();
+        await checkbox.click();
+        await expect(checkbox).toBeChecked();
+        await button.click();
+        await page.waitForFunction(()=> {
+            const check = document.querySelector("#checkbox")
+            const text = document.querySelector("#message")?.textContent;
+            const link = document.querySelector(("button[onclick='swapCheckbox()']"))?.textContent
+
+            return !check && text=="It's gone!" && link == "Add"
+        })
+
+        await button.click();
+        await expect(checkbox).toBeVisible();
+        await expect(checkbox).not.toBeChecked();
+        await expect(message).toBeVisible();
+        await expect(message).toHaveText("It's back!")
+    })
+})
