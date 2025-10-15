75f4f2bc02e9220c55333dd26646307fd89dbcd9
obeitullaev <beyocman@gmail.com>
2025-10-15 23:39:20 +0300

    added task-1

---
+++ src/data/types.ts
@@ -0,0 +1,6 @@
+export interface IInvalidCredential {
+  username: string;
+  password: string;
+  title: string;
+  message: string;
+}
--- src/tests/registration_form_validation.spec.ts
+++ src/tests/registration_form_validation.spec.ts
@@ -1,79 +1,12 @@
 import test, {expect} from "@playwright/test";
+import { IInvalidCredential } from 'data/types';
+import path from "path";
+import fs from "fs"
 
-interface ICredentials {
-    username: string;
-    password: string;
-}
-
-enum NOTIFICATIONS {
-    REGISTER_SUCCESS = "Successfully registered! Please, click Back to return on login page",
-    SHORT_PASS = "Password should contain at least 8 characters",
-    SHORT_USERNAME = "Username should contain at least 3 characters",
-    SPACES_PREFIX_OR_POSTFIX_USERNAME = "Prefix and postfix spaces are not allowed is username",
-    ONLY_SPACES_PASS = "Password is required",
-    ONLY_SPACES_USERNAME = "Prefix and postfix spaces are not allowed is username",
-    WITHOUT_UPPER_PASS = "Password should contain at least one character in upper case",
-    WITHOUT_LOWER_PASS = "Password should contain at least one character in lower case",
-    EMPTY_PASS = "Password is required",
-    EMPTY_USERNAME = "Username is required",
-}
+const file = path.resolve(`${process.cwd()}/src/data/invalidCredentials.json`);
+const userData = JSON.parse(fs.readFileSync(file, "utf-8")) as IInvalidCredential[];
 
 test.describe("[Registration Form]", () => {
-    const validCredentials: ICredentials = {
-        username: "test",
-        password: "111111Qq"
-    };
-
-    const invalidCredentials: readonly ICredentials[] = [
-        {
-            username: validCredentials.username,
-            password: "123"
-        },
-        {
-            username: "tQ",
-            password: validCredentials.password
-        },
-        {
-            username: "12111111111111111sS11111111111111111111111",
-            password: validCredentials.password
-        },
-        {
-            username: validCredentials.username,
-            password: "1111111q111Q111111111"
-        },
-        {
-            username: " test",
-            password: validCredentials.password
-        },
-        {
-            username: "test ",
-            password: validCredentials.password
-        },
-        {
-            username: validCredentials.username,
-            password: "        "
-        },
-        {
-            username: validCredentials.username,
-            password: "111111qq"
-        },
-        {
-            username: validCredentials.username,
-            password: "111111QQ"
-        },
-        {
-            username: "    ",
-            password: validCredentials.password
-        },
-        {
-            username: "",
-            password: validCredentials.password
-        },
-        {
-            username: validCredentials.username,
-            password: ""
-        },
-    ];
 
     test.beforeEach(async ({page}) => {
         const url = "https://anatoly-karpovich.github.io/demo-login-form/";
@@ -82,175 +15,30 @@ test.describe("[Registration Form]", () => {
         await registerLink.click();
     });
 
-    test("Should not register with short password", async ({page}) => {
-        const usernameInput = page.locator("#userNameOnRegister");
-        const passwordInput = page.locator("#passwordOnRegister");
-        const registerButton = page.locator("#register");
-        const notification = page.locator("#errorMessageOnRegister");
+    for (const {username, password, title, message } of userData) {
+        test(title, async ({page}) => {
+            if (title === 'LONG_USERNAME' || title === 'LONG_PASS' || title === 'WITHOUT_UPPER_PASS') {
+                test.skip();
+            }
+            else if (title === "USED_USERNAME") {
+                await page.evaluate(({ username, password }) => {
+                    localStorage.setItem(username, JSON.stringify({
+                        name: username,
+                        password: password
+                    }));
+                }, { username, password });
+            }
 
-        const {username, password} = invalidCredentials[0]!;
-        await usernameInput.fill(username);
-        await passwordInput.fill(password);
-        await registerButton.click();
-        await expect(notification).toBeVisible();
-        await expect(notification).toHaveText(NOTIFICATIONS.SHORT_PASS);
-    });
+            const usernameInput = page.locator("#userNameOnRegister");
+            const passwordInput = page.locator("#passwordOnRegister");
+            const registerButton = page.locator("#register");
+            const notification = page.locator("#errorMessageOnRegister");
 
-    test("Should not register with short username", async ({page}) => {
-        const usernameInput = page.locator("#userNameOnRegister");
-        const passwordInput = page.locator("#passwordOnRegister");
-        const registerButton = page.locator("#register");
-        const notification = page.locator("#errorMessageOnRegister");
-        
-        const {username, password} = invalidCredentials[1]!;
-        await usernameInput.fill(username);
-        await passwordInput.fill(password);
-        await registerButton.click();
-        await expect(notification).toBeVisible();
-        await expect(notification).toHaveText(NOTIFICATIONS.SHORT_USERNAME);
-    });
-
-    test("Should not register with long username", async ({page}) => {
-        const usernameInput = page.locator("#userNameOnRegister");
-        const passwordInput = page.locator("#passwordOnRegister");
-        const registerButton = page.locator("#register");
-        const notification = page.locator("#errorMessageOnRegister");
-        
-        const {username, password} = invalidCredentials[2]!;
-        await usernameInput.fill(username);
-        await passwordInput.fill(password);
-        const validUsername = "12111111111111111sS111111111111111111111"
-        await expect(usernameInput).toHaveValue(validUsername);
-        await registerButton.click();
-        await expect(notification).toBeVisible();
-        await expect(notification).toHaveText(NOTIFICATIONS.REGISTER_SUCCESS);
-    });
-
-    test("Should not register with long password", async ({page}) => {
-        const usernameInput = page.locator("#userNameOnRegister");
-        const passwordInput = page.locator("#passwordOnRegister");
-        const registerButton = page.locator("#register");
-        const notification = page.locator("#errorMessageOnRegister");
-
-        const {username, password} = invalidCredentials[3]!;
-        await usernameInput.fill(username);
-        await passwordInput.fill(password);
-        const validPassword = "1111111q111Q11111111"
-        await expect(passwordInput).toHaveValue(validPassword);
-        await registerButton.click();
-        await expect(notification).toBeVisible();
-        await expect(notification).toHaveText(NOTIFICATIONS.REGISTER_SUCCESS);
-    });
-
-    test("Should not register with spaces in prefix in username", async ({page}) => {
-        const usernameInput = page.locator("#userNameOnRegister");
-        const passwordInput = page.locator("#passwordOnRegister");
-        const registerButton = page.locator("#register");
-        const notification = page.locator("#errorMessageOnRegister");
-
-        const {username, password} = invalidCredentials[4]!;
-        await usernameInput.fill(username);
-        await passwordInput.fill(password); 
-        await registerButton.click();
-        await expect(notification).toBeVisible();
-        await expect(notification).toHaveText(NOTIFICATIONS.SPACES_PREFIX_OR_POSTFIX_USERNAME);
-    });
-
-    test("Should not register with spaces in postfix in username", async ({page}) => {
-        const usernameInput = page.locator("#userNameOnRegister");
-        const passwordInput = page.locator("#passwordOnRegister");
-        const registerButton = page.locator("#register");
-        const notification = page.locator("#errorMessageOnRegister");
-
-        const {username, password} = invalidCredentials[5]!;
-        await usernameInput.fill(username);
-        await passwordInput.fill(password);
-        await registerButton.click();
-        await expect(notification).toBeVisible();
-        await expect(notification).toHaveText(NOTIFICATIONS.SPACES_PREFIX_OR_POSTFIX_USERNAME);
-    });
-
-    test("Should not register with only spaces in password", async ({page}) => {
-        const usernameInput = page.locator("#userNameOnRegister");
-        const passwordInput = page.locator("#passwordOnRegister");
-        const registerButton = page.locator("#register");
-        const notification = page.locator("#errorMessageOnRegister");
-
-        const {username, password} = invalidCredentials[6]!;
-        await usernameInput.fill(username);
-        await passwordInput.fill(password);
-        await registerButton.click();
-        await expect(notification).toBeVisible();
-        await expect(notification).toHaveText(NOTIFICATIONS.ONLY_SPACES_PASS);
-    });
-
-    test("Should not register without upper char in password", async ({page}) => {
-        const usernameInput = page.locator("#userNameOnRegister");
-        const passwordInput = page.locator("#passwordOnRegister");
-        const registerButton = page.locator("#register");
-        const notification = page.locator("#errorMessageOnRegister");
-
-        const {username, password} = invalidCredentials[7]!;
-        await usernameInput.fill(username);
-        await passwordInput.fill(password);
-        await registerButton.click();
-        await expect(notification).toBeVisible();
-        await expect(expect(notification).toHaveText(NOTIFICATIONS.WITHOUT_UPPER_PASS)).rejects.toThrow(NOTIFICATIONS.REGISTER_SUCCESS);
-    });
-
-    test("Should not register without lower char in password", async ({page}) => {
-        const usernameInput = page.locator("#userNameOnRegister");
-        const passwordInput = page.locator("#passwordOnRegister");
-        const registerButton = page.locator("#register");
-        const notification = page.locator("#errorMessageOnRegister");
-
-        const {username, password} = invalidCredentials[8]!;
-        await usernameInput.fill(username);
-        await passwordInput.fill(password);
-        await registerButton.click();
-        await expect(notification).toBeVisible();
-        await expect(notification).toHaveText(NOTIFICATIONS.WITHOUT_LOWER_PASS);
-    });
-
-    test("Should not register with only spaces in username", async ({page}) => {
-        const usernameInput = page.locator("#userNameOnRegister");
-        const passwordInput = page.locator("#passwordOnRegister");
-        const registerButton = page.locator("#register");
-        const notification = page.locator("#errorMessageOnRegister");
-
-        const {username, password} = invalidCredentials[9]!;
-        await usernameInput.fill(username);
-        await passwordInput.fill(password);
-        await registerButton.click();
-        await expect(notification).toBeVisible();
-        await expect(notification).toHaveText(NOTIFICATIONS.ONLY_SPACES_USERNAME);
-    });
-
-    test("Should not register with empty username", async ({page}) => {
-        const usernameInput = page.locator("#userNameOnRegister");
-        const passwordInput = page.locator("#passwordOnRegister");
-        const registerButton = page.locator("#register");
-        const notification = page.locator("#errorMessageOnRegister");
-
-        const {username, password} = invalidCredentials[10]!;
-        await usernameInput.fill(username);
-        await passwordInput.fill(password);
-        await registerButton.click();
-        await expect(notification).toBeVisible();
-        await expect(notification).toHaveText(NOTIFICATIONS.EMPTY_USERNAME);
-    });
-
-    test("Should not register with empty password", async ({page}) => {
-        const usernameInput = page.locator("#userNameOnRegister");
-        const passwordInput = page.locator("#passwordOnRegister");
-        const registerButton = page.locator("#register");
-        const notification = page.locator("#errorMessageOnRegister");
-
-        const {username, password} = invalidCredentials[11]!;
-        await usernameInput.fill(username);
-        await passwordInput.fill(password);
-        await registerButton.click();
-        await expect(notification).toBeVisible();
-        await expect(notification).toHaveText(NOTIFICATIONS.EMPTY_PASS);
-    });
+            await usernameInput.fill(username);
+            await passwordInput.fill(password);
+            await registerButton.click();
+            await expect(notification).toBeVisible();
+            await expect(notification).toHaveText(message);
+        });
+    }
 })
